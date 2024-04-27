36729e8063d19c19b2a0ac3f2d42d3aa80f4bf0b
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2024-04-27 14:02:56 +0530

    Remove resend & github access

--- auth.config.ts
+++ auth.config.ts
@@ -13,41 +13,5 @@ export default {
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
-    // Email({
-    //   sendVerificationRequest: async ({ identifier, url, provider }) => {
-    //     const user = await getUserByEmail(identifier);
-    //     if (!user || !user.name) return null;
-
-    //     const userVerified = user?.emailVerified ? true : false;
-    //     const authSubject = userVerified ? `Sign-in link for ${siteConfig.name}` : "Activate your account";
-
-    //     try {
-    //       const { data, error } = await resend.emails.send({
-    //         from: 'SaaS Starter App <onboarding@resend.dev>',
-    //         to: process.env.NODE_ENV === "development" ? 'delivered@resend.dev' : identifier,
-    //         subject: authSubject,
-    //         react: MagicLinkEmail({
-    //           firstName: user?.name as string,
-    //           actionUrl: url,
-    //           mailType: userVerified ? "login" : "register",
-    //           siteName: siteConfig.name
-    //         }),
-    //         // Set this to prevent Gmail from threading emails.
-    //         // More info: https://resend.com/changelog/custom-email-headers
-    //         headers: {
-    //           'X-Entity-Ref-ID': new Date().getTime() + "",
-    //         },
-    //       });
-
-    //       if (error || !data) {
-    //         throw new Error(error?.message)
-    //       }
-
-    //       // console.log(data)
-    //     } catch (error) {
-    //       throw new Error("Failed to send verification email.")
-    //     }
-    //   },
-    // }),
   ],
 } satisfies NextAuthConfig
